87be9852fae57eb309321b6b4eed71758fa3f0fe
Keasser Dune <gonzaleskeasser@gmail.com>
2026-05-07 17:26:52 +0800

    Added login test validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,6 +12,12 @@ exports.helloAuth = (req, res) => {
 exports.loginTest = (req, res) => {
   const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "❌ Email and password are required",
+    });
+  }
+
   res.json({
     message: "✅ POST request working",
     email,
